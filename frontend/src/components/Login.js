14c6648c685937eade9b2a207adb72641f61f5f2
vanyaadev <detskov5@gmail.com>
2019-11-26 23:26:49 +0100

    Add Login

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+	state = {
+		username: '',
+		password: ''
+	};
+	handleEmailChange = e => {
+		this.setState({ username: e.target.value });
+	};
+
+	handlePasswordChange = e => {
+		this.setState({ password: e.target.value });
+	};
+
+	render() {
+		return (
+			<form>
+				<input
+					type='text'
+					name='username'
+					placeholder='Username '
+					value={this.state.username}
+					onChange={this.handleEmailChange}
+				/>
+				<input
+					type='password'
+					name='password'
+					placeholder='Password'
+					value={this.state.password}
+					onChange={this.handlePasswordChange}
+				/>
+				<button type='button' onClick={this.handleLogin}>
+					Login
+				</button>
+			</form>
+		);
+	}
+
+	handleLogin = () => {
+		fetch('http://localhost:8080/authentication', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(this.state)
+		});
+	};
+}
